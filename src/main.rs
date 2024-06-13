98741078de845239c00a84112093629d59429e00
devim <developerimalka@gmail.com>
2024-06-13 22:08:14 +0530

    Base stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{Write, stdout};
+use crossterm::{ExecutableCommand};
+use crossterm::terminal::{self, ClearType};
+use std::time::Duration;
+use std::thread;
+
+fn main(){
+    let mut stdout = stdout();
+    stdout.execute(terminal::Clear(ClearType::All)).unwrap();
+    thread::sleep(Duration::from_secs(5));
+    let _ = stdout.flush();
 }
